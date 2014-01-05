641df5ca110716be47ede233f477c25b04435f4d
Benjamin Bergman <bbergman@tenable.com>
2014-01-04 18:47:48 -0600

    Add new symbol to other graphics models

--- graph.c
+++ graph.c
@@ -478,6 +478,10 @@ graph_symbol_to_chtype(struct graph_symbol *symbol)
 		if (!symbol->vbranch)
 			graphics[0] = ' ';
 		graphics[1] = ACS_VLINE;
+		if (symbol->collapse) {
+			graphics[0] = ' ';
+			graphics[1] = ACS_ULCORNER;
+		}
 		return graphics;
 	}
 
@@ -516,6 +520,8 @@ graph_symbol_to_ascii(struct graph_symbol *symbol)
 		}
 		if (symbol->vbranch)
 			return "-|";
+		if (symbol->collapse)
+			return " .";
 		return " |";
 	}
 
